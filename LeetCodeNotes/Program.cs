897bd3618043f5f7527a75bcd34d3a1ce3629d1b
Danielle Siazon <dssiazon@gmail.com>
2023-11-06 17:59:38 -0600

    leet code notes

---
+++ LeetCodeNotes/Program.cs
@@ -0,0 +1,12 @@
+﻿public static T GetValue<T>(this XElement e, string name, T def_value = ) {
+    if (e != null) {
+        XElement x = e.Elements(XName.Get(name)).FirstOrDefault();
+        if (x != null && !string.IsNullOrWhiteSpace(x.Value.Trim())) {
+			return ______________________________________________;
+		}
+	}
+	return def_value;
+
+
+
+
